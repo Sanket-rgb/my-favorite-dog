fcb6972efcb8c6c91bf5e254e8b3d5717014e935
Sanket Koli <kolisanket15@gmail.com>
2023-07-03 00:06:06 -0700

    Created endpoints file

--- src/API/endpoints.js
+++ src/API/endpoints.js
@@ -4,3 +4,9 @@ export const baseUrl = "https://frontend-take-home-service.fetch.com";
 // Authentication endpoints
 export const loginEndpoint = "/auth/login";
 export const logoutEndpoint = "/auth/logout";
+
+// Data fetching endpoints
+export const dogsBreedsEndpoint = "/dogs/breeds";
+export const dogsSearchEndpoint = "/dogs/search";
+export const dogsEndpoint = "/dogs";
+export const dogsMatchEndpoint = "/dogs/match";
