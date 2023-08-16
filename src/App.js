b07c86030286c1f6d8daced2de288b89d6d86807
Sanket Koli <kolisanket15@gmail.com>
2023-08-15 17:52:28 -0700

    Use RootLayout, ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage";
 import HomePage from "./Pages/HomePage";
-import Login from "./Pages/Login";
+import LoginPage, { action as loginAction } from "./Pages/LoginPage";
+import RootLayout from "./Pages/Root";
 
 function App() {
   const router = createBrowserRouter([
-    { index: true, element: <Login /> },
     {
-      path: "home",
-      element: <HomePage />,
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <LoginPage />,
+          action: loginAction,
+        },
+        {
+          path: "home",
+          element: <HomePage />,
+        },
+      ],
     },
   ]);
 
